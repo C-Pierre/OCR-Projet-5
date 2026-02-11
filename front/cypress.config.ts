014cafaf0793cf728ff5eeedd9b45b3f88de17b0
C-Pierre <pierre.cistac@gmail.com>
2026-02-11 14:58:56 +0100

    Project 5 | US-14 : Documentation

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,23 +1,12 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  videosFolder: 'cypress/videos',
-  screenshotsFolder: 'cypress/screenshots',
-  fixturesFolder: 'cypress/fixtures',
-  video: false,
-
   e2e: {
     baseUrl: 'http://localhost:4200',
-    reporterOptions: {
-      reportDir: "cypress/reports",
-      overwrite: false,
-      html: false,
-      json: true,
-    },
     setupNodeEvents(on, config) {
-      const codeCoverageTask = require('@cypress/code-coverage/task')
-      codeCoverageTask(on, config)
-      return config
+      const codeCoverageTask = require('@cypress/code-coverage/task');
+      codeCoverageTask(on, config);
+      return config;
     },
   },
-})
+});
